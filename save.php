198446745e8f09d7256a5c24ad8ce964b2f6e74f
Rebecca Cremona <rebecca_cremona@harvard.edu>
2015-05-04 15:38:11 -0400

    Use PDO in save.php (still non-securely gets path from $POST, however).

--- save.php
+++ save.php
@@ -10,19 +10,20 @@ if ( !isset($_POST["data"])) {
 	
 	// Get the data
 	$data = json_decode($_POST["data"]);
+	
+	$path = $data[3];
+	$db = new PDO('sqlite:' . $path);
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
-    $column_safe = SQLite3::escapeString($data[0]);
-    $id_safe = SQLite3::escapeString($data[1]);
-    $content_safe = SQLite3::escapeString($data[2]);
-    $path = $data[3];
-    $table_safe = SQLite3::escapeString($data[4]);
+	$column_safe = $db->quote($data[0]);
+	$table_safe = $db->quote($data[4]);
 
-	// Open the database  
-	$db = new SQLite3($path);
+    $id = $data[1];
+    $content= $data[2];
       
 	if(!$db){
 	  $response['error'] = TRUE;
-	  $response['message'] = $db->lastErrorMsg();
+	  $response['message'] = $db->errorInfo();
 	  echo json_encode($response);
 	  die();
 	}
@@ -30,18 +31,24 @@ if ( !isset($_POST["data"])) {
 	// Update the database.
       
 	// ...create a new row, or update existing one
-	$exists = $db->querySingle("SELECT id FROM {$table_safe} where id = {$id_safe}");
+	$sql_exists = $db->prepare("SELECT id FROM {$table_safe} WHERE id = :id");
+	$sql_exists->execute(array('id' => $id));
+	$sql_exists->rowCount() ? true : false;
+	$exists = $sql_exists->fetch(PDO::FETCH_ASSOC);
 
 	if ($exists){
-	$update = $db->exec("UPDATE {$table_safe} SET {$column_safe} = '{$content_safe}' WHERE id = {$id_safe}");
+	$sql_update = $db->prepare("UPDATE {$table_safe} SET {$column_safe} = :content WHERE id = :id");
+	$update = $sql_update->execute(array(':id' => $id, ':content' => $content));
+
 	} else {
-	$update = $db->exec("INSERT INTO {$table_safe} (id, {$column_safe}) VALUES ({$id_safe},'{$content_safe}')");
+	$sql_insert = $db->prepare("INSERT INTO {$table_safe} (id, {$column_safe}) VALUES (:id, :content)");
+	$update = $sql_insert->execute(array(':id' => $id, ':content' => $content));
 	}
       
 	// ...check for errors
 	if(!$update){
 		$response['error'] = TRUE;
-		$response['message'] = $db->lastErrorMsg();
+		$response['message'] = $db->errorInfo();
 		echo json_encode($response);
 		die();
 	} else {
@@ -50,9 +57,5 @@ if ( !isset($_POST["data"])) {
 		echo json_encode($response);
 	}
 }
-
-// Close the database
-$db->close();
-
 
 ?> 
